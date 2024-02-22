846434daab882629f15aeb4ad78b5fdf01ca3081
Ayush Nagar <ayushnagar15tfe@gmail.com>
2024-02-22 23:49:24 +0530

    Used compound component pattern to build reusable components

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -8,7 +8,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
     const { id: editId, ...editValues } = cabinToEdit;
     const isEditSession = Boolean(editId);
 
@@ -38,6 +38,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 {
                     onSuccess: () => {
                         reset();
+                        onCloseModal?.();
                     },
                 }
             );
@@ -47,13 +48,17 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 {
                     onSuccess: () => {
                         reset();
+                        onCloseModal?.();
                     },
                 }
             );
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form
+            onSubmit={handleSubmit(onSubmit)}
+            type={onCloseModal ? "modal" : "regular"}
+        >
             <FormRow label="Cabin name" error={errors?.name?.message}>
                 <Input
                     type="text"
@@ -145,7 +150,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button
+                    variation="secondary"
+                    type="reset"
+                    onClick={() => onCloseModal?.()}
+                >
                     Cancel
                 </Button>
                 <Button disabled={isWorking}>
